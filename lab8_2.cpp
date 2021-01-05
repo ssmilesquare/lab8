e8a548f2b420f0ab9d853e60641bc13c577c9f8e
ssmilesquare <ssmilesquare@gmail.com>
2021-01-05 16:39:13 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N,int M){
+	if(N <= 0 || M <= 0) cout << "Invalid input";
+	else for(int i = 0;i < N;i++){
+		for (int k = 0; k < M; k++){
+			cout << "O";
+		}
+		cout << endl;
+	}
+}
 
 int main(){
-	
 	printO(2,2);
 	cout << "\n";
-	
 	printO(3,5);
 	cout << "\n";	
-	
 	printO(5,3);
 	cout << "\n";	
-	
 	printO(0,3);
 	cout << "\n";	
-	
 	printO(7,-1);
 	cout << "\n";	
 	
